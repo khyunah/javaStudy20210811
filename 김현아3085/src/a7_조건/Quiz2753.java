e0413b28fdc6381df0877efbf171fc973f65d146
= <=>
2021-08-18 21:11:26 +0900

    remove result

--- 김현아3085/src/a7_조건/Quiz2753.java
+++ 김현아3085/src/a7_조건/Quiz2753.java
@@ -8,10 +8,10 @@ public class Quiz2753 {
 		Scanner in = new Scanner(System.in);
 		//해당년도 4의배수이면서 100의 배수가 아니거나 400의 배수이다.
 		int year = in.nextInt();
-		int result;
+	
 		
 		if (year%4 ==0 && year%100!= 0 || year%400 ==0) {
-			result = 1;
+		
 			System.out.println("윤년입니다.!!");		
 		}else {
 			System.out.println("윤년이 아닙니다.!!");
