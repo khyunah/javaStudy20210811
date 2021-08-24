402c0e22ac1b15b117f8df9757dec0e2f9d6558c
UserK <UserK@Win10-01>
2021-08-24 23:52:34 +0900

    2021 08 24 homework1

---
+++ 김현아3085/src/a8_반복/HomeWork.java
@@ -0,0 +1,63 @@
+package a8_นÝบน;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		
+		for(i=0; i<5; i++) {
+			for(int j = 0; j<i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(i=0; i<5;i++) {
+			for(int j = 0; j+5>i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(i=0;i<5;i++) {				
+			for(int j = 0; j+5>i+1;j--) {								
+			System.out.print(' ');				
+			}
+			for(int k = 0; k<i+1;k++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for(i=0;i<5;i++) {
+			for(int j = 0; j<i;j++) {
+			System.out.print(' ');
+			}
+			for(int k=0; k+5>i; k--) {
+			System.out.print("*");		
+			}
+			System.out.println();			
+		}
+		System.out.println();
+	
+		for(i=0; i<5;i++) {
+			for(int j=0;j<4-i;j++) {
+			System.out.print(" ");
+			}			
+			for(int k = 0; k<i+(i+1);k++) {				
+			System.out.print("*");			
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+
+
+	
+
+	}
+}
