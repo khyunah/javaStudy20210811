cd612a18ee329c03c828b1e76ba9968a7742bae7
= <fj2008@naver.com>
2021-09-27 21:54:50 +0900

    2021 09 27

---
+++ 김현아3085/src/b20_데이터베이스/LoginDaoImpl.java
@@ -0,0 +1,78 @@
+package b20_데이터베이스;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import db.DBConnectionMgr;
+
+public class LoginDaoImpl implements LoginDao{
+	private DBConnectionMgr pool;
+	
+	public LoginDaoImpl() {
+		pool = DBConnectionMgr.getInstance();
+	}
+	
+	//로그인 로직
+	public int login(String id, String password) {
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int flag =0;
+		
+		try {
+			con = pool.getConnection();
+			sql = "select count(um.user_id), count(ud.user_password) from "
+					+ "user_mst um "
+					+ "left outer join user_mst ud on (ud.user_id= um.user_id and ud.user_password = ?)"
+					+ "where um.user_id = ?";
+			
+			pstmt = con.prepareStatement(sql);
+			                   //마리아디비가 알아들을수잇게 바꿔주는 애
+			pstmt.setString(1, password);
+			pstmt.setString(2, id);
+			//결과값출력
+			rs = pstmt.executeQuery();
+			
+			rs.next();
+			flag = rs.getInt(1)+ rs.getInt(2);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		
+		return flag;
+	}
+
+	@Override
+	public String getLoginUserName(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String name =null;
+		
+		try {
+			con = pool.getConnection();
+			sql = "select user_name from user_mst where user_id=?";
+			pstmt = con.prepareStatement(sql);
+			                   //마리아디비가 알아들을수잇게 바꿔주는 애
+			pstmt.setString(1, id);
+			//결과값출력
+			rs = pstmt.executeQuery();
+			
+			rs.next();
+			name = rs.getString(1);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return name;
+	}
+}
